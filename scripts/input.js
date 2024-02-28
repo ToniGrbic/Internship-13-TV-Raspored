af7aae485b746d1343bfdd119e82cceba868829d
ToniGrbic <tonigrbic.5@gmail.com>
2024-02-28 19:13:13 +0100

    set parentPIN on content loaded

--- scripts/input.js
+++ scripts/input.js
@@ -38,6 +38,10 @@ function getParentPIN() {
   return localStorage.getItem("parentPIN");
 }
 
+window.addEventListener("DOMContentLoaded", () => {
+  parentPIN = Number(getParentPIN());
+});
+
 export {
   setParentPIN,
   changeParentPIN,
