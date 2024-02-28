77b84b573394ebac1e94d48de4f799197aac7e3d
ToniGrbic <tonigrbic.5@gmail.com>
2024-02-29 00:00:55 +0100

    fixes for input pin and pin validation

--- scripts/input.js
+++ scripts/input.js
@@ -1,33 +1,39 @@
 let parentPIN;
 
-function setParentPIN(message) {
-  const newParentPIN = Number(prompt(message));
-  if (
-    isNaN(newParentPIN) ||
-    newParentPIN.length < 4 ||
-    newParentPIN.length > 8
-  ) {
-    alert("Invalid PIN format, please try again.");
-    setParentPIN();
-  } else {
+function setParentPIN(message, oldPIN = null) {
+  const newPIN = prompt(message);
+  if (isValidPIN(oldPIN, newPIN)) {
     alert("Parent PIN set successfully.");
-    parentPIN = newParentPIN;
+    parentPIN = newPIN;
     localStorage.setItem("parentPIN", parentPIN);
+  } else {
+    alert("Invalid PIN format, please try again.");
+    setParentPIN(message, oldPIN);
   }
+}
+
+function isValidPIN(oldPIN, pin) {
+  let isValid = !isNaN(pin) || pin.length < 4 || pin.length > 8;
+  if (!oldPIN) return isValid;
+
+  isValid = isValid && pin !== oldPIN;
+  if (!isValid) alert("New PIN cannot be the same as the old PIN.");
+
+  return isValid;
 }
 
 function changeParentPIN() {
   const oldPIN = prompt("Please enter the old parent PIN.");
-  if (Number(oldPIN) !== parentPIN) {
+  if (oldPIN !== parentPIN) {
     alert("Incorrect PIN. Access denied.");
     return;
   }
-  setParentPIN("Please provide a new parent PIN. (4-8 digits)");
+  setParentPIN("Please provide a new parent PIN. (4-8 digits)", oldPIN);
 }
 
 function inputParentPIN() {
   const pin = prompt("Please enter the parent PIN to view this program.");
-  if (Number(pin) !== parentPIN) {
+  if (pin !== parentPIN) {
     alert("Incorrect PIN. Access denied.");
     return false;
   }
@@ -39,7 +45,7 @@ function getParentPIN() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  parentPIN = Number(getParentPIN());
+  parentPIN = getParentPIN();
 });
 
 export {
